33b8a8efc2ea58758d75f4b0cd5bb775748f3c9d
MatheusG <mariaoglanowski@gmail.com>
2025-07-02 22:09:22 -0300

    JavaScript

---
+++ event.js
@@ -0,0 +1,16 @@
+function mouseOut()
+{
+    document.imagem.src ="imagens/cat02.png";
+}
+function mouseOut()
+{
+    document.imagem.src ="imagens/cat01.png";
+}
+function mouseOut()
+{
+    document.imagem.src ="imagens/cat03.png";
+}
+function mouseOut()
+{
+    document.imagem.src ="imagens/cat04.png";
+}
